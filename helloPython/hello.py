3f1074ea1d25e3ba6e74ac658ff6c30302e655e3
aslan <aslanchik19977777@gmail.com>
2022-12-19 18:03:43 +0300

    1 seminar

---
+++ helloPython/hello.py
@@ -0,0 +1,36 @@
+# является ли первое число квадратом другого и наобарот
+
+#n = int(input())
+#m = int(input())
+
+# if n == m ** 2 or m == n ** 2:
+#   print("YES")
+# else:
+#   print("NO")
+
+
+# какое число максимальное
+
+#max = 0
+# for i in range(5):
+#   nam = int(input())
+#  if max < nam:
+#    max = nam
+# print(max)
+
+
+# принимает число дробное и показывает первую цифру дробной части
+
+#a = float(input("input"))
+#num = int(a *10 % 10)
+# print(num)
+
+
+# принимаем число и проверяем равно ли оно 5, 10 или 15 но не 30
+
+#a = int(input("input"))
+#if (a % 5 == 0 and a % 10 == 0 or a % 15 == 0) and a % 30 != 0:
+#    print("yes")
+#else:
+ #   print("no")
+  
